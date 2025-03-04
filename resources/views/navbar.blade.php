72ea52d27ac56484158d9b0f9724fc314e4ef5da
paiboonmt <paiboon500@gmail.com>
2025-03-04 10:47:49 +0700

    update

---
+++ resources/views/navbar.blade.php
@@ -0,0 +1,39 @@
+<div class="container-fluid">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Feelfree Transport</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Conditions</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">FAQ</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Vehiclerental</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About us</a>
+                    </li>
+
+
+                </ul>
+
+            </div>
+        </div>
+    </nav>
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,72 +9,37 @@
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+    {{-- <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" /> --}}
     {{-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"> --}}
-
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('resources/css/google-font.css') }}">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
 </head>
 
 <body>
-    <div class="container-fluid">
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">Feelfree Transport</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+    @include('navbar')
 
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Conditions</a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">FAQ</a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Vehiclerental</a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Contact</a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">About us</a>
-                        </li>
-
-
-                    </ul>
-
-                </div>
-            </div>
-        </nav>
-    </div>
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-12">
-          <img src="https://central-van.com/wp-content/uploads/2022/01/cv5.jpg" width="100%" height="20%" alt="Feelfree Transport">
-        </div>
-      </div>
-    </div>
-    <div class="container mt-3">
+    <div class="container mt-2">
         <div class="row">
-            <div class="col-md-4 p-1" style="background-color: rgb(4, 192, 186); color: white; border-radius: 10px;">
+            <div class="col-md-12 col-sm-12 p-1" style="background-color: rgb(3, 172, 166); color: white; border-radius: 5px;">
                 <h1 class="text-center">Feelfree Transport</h1>
-                <p>Feelfree Transport is a car rental company that offers</p>
-            </div>
-            <div class="col-md-6 p-1">
-                <img src="https://via.placeholder.com/500" alt="Feelfree Transport" class="img-fluid">
+                <div class="text-center">
+                    <p>Feelfree Transport is a car rental company that offers</p>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, unde?
+                    </p>
+                </div>
             </div>
         </div>
     </div>
 </body>
+
+
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
